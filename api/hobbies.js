0ce994804bfb21ac3647788974b5058dca3fc054
Dante Munoz <damunoz32@gmail.com>
2025-07-15 17:37:50 -0500

    added User-Agent to accepted headers

--- api/hobbies.js
+++ api/hobbies.js
@@ -4,7 +4,7 @@ module.exports = async (req, res) => {
     
     res.setHeader('Access-Control-Allow-Origin', 'https://dantemunoz.webflow.io');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, User-Agent');
 
     // Handle preflight OPTIONS request for CORS
     if (req.method === 'OPTIONS') {
